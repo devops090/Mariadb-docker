c3ea8c8f5752960bcb898160e84023f382f61dda
root <root@maridadb.us-central1-a.c.helpful-pixel-240707.internal>
2019-10-13 11:41:27 +0000

    jkfhhdfbdjskf

--- start.sh
+++ start.sh
@@ -2,16 +2,28 @@
 #systemctl stop mariadb
 #chown -R mysql:mysql /var/lib/mysql/mysql.sock
 #chmod 777 /var/lib/mysql/mysql.sock
-sudo /usr/libexec/mysqld stop
-#RUN mysqld_safe --skip-grant-tables &
+#sudo /usr/libexec/mysqld stop
+#mysqld_safe start --skip-grant-tables & 
+#wait 15
 #RUN /usr/libexec/mysqld enable
 mysql -e "CREATE USER 'student'@'%' IDENTIFIED BY 'student@1';"
 mysql -e "grant all privileges on studentapp.* to 'student'@'%' identified by 'student@1';"
 mysql -e "flush privileges;"
 #mysql -e "quit;"
-#systemctl stop mariadb
+systemctl stop mariadb
+sleep 10
+killall -9 mysqld mysqld_safe
+sleep 10
 mysqld_safe --skip-grant-tables &
-sudo /usr/libexec/mysqld enable
+sleep 10
+#wait 15
+#sudo /usr/libexec/mysqld enable
 #systemctl stop mariadb
-#systemctl enable mariadb
+#sleep 10
+#wait 15
+systemctl enable mariadb
+sleep 10
+#wait 15
+#mysql -e "CREATE USER 'student'@'%' IDENTIFIED BY 'student@1';"
+#mysql -e "grant all privileges on studentapp.* to 'student'@'%' identified by 'student@1';"
 mysql -ustudent -pstudent@1 < studentapp.sql
